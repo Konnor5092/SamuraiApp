c0b36a1a928072b0f87c17ced827065378d0873f
Matt O'Connor <matthew.oconnor@smartapprentices.com>
2020-03-09 19:36:59 +0000

    Packages and cleanup

--- SamuraiAPI/Controllers/WeatherForecastController.cs
+++ SamuraiAPI/Controllers/WeatherForecastController.cs
@@ -24,16 +24,13 @@ namespace SamuraiAPI.Controllers
         }
 
         [HttpGet]
-        public IEnumerable<WeatherForecast> Get()
+        public IEnumerable<string> Get()
         {
-            var rng = new Random();
-            return Enumerable.Range(1, 5).Select(index => new WeatherForecast
-            {
-                Date = DateTime.Now.AddDays(index),
-                TemperatureC = rng.Next(-20, 55),
-                Summary = Summaries[rng.Next(Summaries.Length)]
-            })
-            .ToArray();
+            var stringList = new List<string>() {
+                "Hello", "What", "Is", "This"
+            };
+
+            return stringList;
         }
     }
 }
